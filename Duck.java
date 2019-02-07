114da24dcbbd28b57c5ca3e5dccbf32e2225f9a5
Alkatib <salkatib@UMICH.EDU>
2019-02-07 14:39:09 -0500

    test message

---
+++ Duck.java
@@ -0,0 +1,13 @@
+Public class duck
+{
+<<<<<<< HEAD
+public fly() {system.out.println("A duck can fly");}
+
+public swim(){system.out.println("A duck can swim");
+=======
+public fly() {system.out.println("A duck can fly");}
+
+public eat(){system.out.println("A duck can eat");}}
+>>>>>>> Bob
+
+public quack(){system.out.println("A duck quacks");}
